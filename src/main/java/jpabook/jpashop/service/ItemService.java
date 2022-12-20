32e48b1404f3e3621ec09ee79ac153adb53410cb
sujin-create <qsoojinq@naver.com>
2022-12-21 03:26:13 +0900

    Add : item controller

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Item.Book;
 import jpabook.jpashop.domain.Item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
-    }
+    }//merge를 사용하는 방법
+
+    @Transactional
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);//findItem에 영속성이 생김 -> 변경 감지를 통해 자동으로 commit된후 flush되어 적용됨
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+
+        return findItem;
+    }//변경감지를 사용하는 방법 -> null update 가능성을 없앰
 
     public List<Item> findItems(){
         return itemRepository.findAll();
